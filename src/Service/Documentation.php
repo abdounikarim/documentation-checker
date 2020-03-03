ef8baf143cb4ecb0ad4e35f9519f02a1aaa010d7
abdounikarim <abdounikarim@gmail.com>
2020-03-03 16:42:25 +0100

    use Symfony HttpClient Component

--- src/Service/Documentation.php
+++ src/Service/Documentation.php
@@ -2,10 +2,14 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpClient\HttpClient;
+
 class Documentation
 {
     public function getContents()
     {
-
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.github.com/repos/symfony/symfony-docs');
+        return $response->toArray();
     }
 }
