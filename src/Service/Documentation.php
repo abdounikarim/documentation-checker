03b2a025f4f1fb535bee5ab888fdc4971cdf5bfb
abdounikarim <abdounikarim@gmail.com>
2020-03-03 17:09:15 +0100

    complete Documentation Service

--- src/Service/Documentation.php
+++ src/Service/Documentation.php
@@ -2,14 +2,48 @@
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\ScopingHttpClient;
 
 class Documentation
 {
+    private $connection;
+    /**
+     * @var ContainerBagInterface
+     */
+    private $containerBag;
+
+    public function __construct(ContainerBagInterface $containerBag)
+    {
+        $this->containerBag = $containerBag;
+    }
+
+    protected function checkConnection()
+    {
+        if($this->connection) {
+            return $this->connection;
+        }
+        return $this->connect();
+    }
+
+    private function connect()
+    {
+        $this->connection = HttpClient::create();
+        $this->connection = new ScopingHttpClient($this->connection, [
+            'https://api\.github\.com/' => [
+                'headers' => [
+                    'Accept' => 'application/vnd.github.v3+json',
+                    'Authorization' => 'token '.$this->containerBag->get('github_token'),
+                ],
+            ]
+        ]);
+        return $this->connection;
+    }
+
     private function getContents()
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'https://api.github.com/repos/symfony/symfony-docs/contents');
+        $response = $this->checkConnection()->request('GET', 'https://api.github.com/repos/symfony/symfony-docs/contents');
         return $response->toArray();
     }
 
@@ -20,8 +54,14 @@ class Documentation
             $length = strlen($file['name']);
             $extension = substr($file['name'], $length - 3);
             if($extension === 'rst') {
-                dump($file);
+                $this->getContent($file);
             }
         }
+    }
+
+    public function getContent($file)
+    {
+        $response = $this->checkConnection()->request('GET', $file['url']);
+        dd($response->toArray());
     }
 }
